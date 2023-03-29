3f9c808b46b321c4da25e3ed6d7914961e0fb23d
szq <szq@qq.com>
2023-03-29 17:24:53 +0800

    master commit2

--- src/main/java/com/szq/gittest/GitTestApplication.java
+++ src/main/java/com/szq/gittest/GitTestApplication.java
@@ -11,6 +11,7 @@ public class GitTestApplication {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("master test");
     }
 
 }
