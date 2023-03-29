8f71ea2ad25070ebb4efc70b4bd729ee71515358
wuzqwe <105253943+wuzqwe@users.noreply.github.com>
2023-03-29 22:11:16 +0800

    pull commit

--- src/main/java/com/szq/gittest/GitTestApplication.java
+++ src/main/java/com/szq/gittest/GitTestApplication.java
@@ -14,6 +14,7 @@ public class GitTestApplication {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 
 }
