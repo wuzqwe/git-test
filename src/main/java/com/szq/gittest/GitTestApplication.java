3279e03c7515ef76d424a68b6fa4b714244a450b
szq <szq@qq.com>
2023-03-29 17:22:07 +0800

    hot-fix commit3

--- src/main/java/com/szq/gittest/GitTestApplication.java
+++ src/main/java/com/szq/gittest/GitTestApplication.java
@@ -11,6 +11,6 @@ public class GitTestApplication {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hot-fix test");
     }
-
 }
