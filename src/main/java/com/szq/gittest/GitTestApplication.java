416ab1c3346f4bc57723d5d7e3f110b303f206ac
szq <szq@qq.com>
2023-03-29 22:06:06 +0800

    push commit3

--- src/main/java/com/szq/gittest/GitTestApplication.java
+++ src/main/java/com/szq/gittest/GitTestApplication.java
@@ -13,6 +13,7 @@ public class GitTestApplication {
         System.out.println("hello git3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 
 }
